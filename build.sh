b61aa17cbdb624e23f8b2f689e3b2ec170f69db6
Jean Fontena <diegofontena@gmail.com>
2019-06-02 19:50:30 -0300

    Add build script

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# This should be a Makefile
+
+FOLDERS="src \
+         src/csv"
+
+PROJECT_DIR="/home/jeandiego/UFRGS/CPD/movielens"
+BIN_DIR="$PROJECT_DIR/build"
+CC=g++
+CFLAGS="-std=c++17 -Wall -Wextra -Werror -Wpedantic"
+
+mkdir -p $BIN_DIR
+cd $PROJECT_DIR
+
+for folder in $FOLDERS
+do
+    cd $folder
+    for file in *.cpp
+    do
+        echo "Building $folder/$file"
+        g++ -c $file $CFLAGS -o $BIN_DIR/$file.o
+    done
+    cd $PROJECT_DIR
+done
+
+OBJECTS="$(ls $BIN_DIR/*.o)"
+g++ $OBJECTS -o $PROJECT_DIR/movielens
+echo "Done."          
